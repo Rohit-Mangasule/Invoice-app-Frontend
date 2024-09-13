ef932f6465568d6a7a92824fc8e7a76e848e9514
Rohit-Mangasule <mangasuler@gmail.com>
2024-09-13 21:13:18 +0530

    Index

--- src/Index.jsx
+++ src/Index.jsx
@@ -13,7 +13,7 @@ const Index = () => {
             transactions with complete transparency and efficiency.
           </p>
 
-          <div className="mt-5">
+          <div className="mt-1">
             <img
               src="https://plus.unsplash.com/premium_photo-1679923814036-8febf10a04c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="Invoicing app"
