a7be1f992eb95b7f542420554d21dea8094b9855
Rohit-Mangasule <mangasuler@gmail.com>
2024-09-14 14:48:39 +0530

    Remove unused Index component and index.css file

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-// import Index from './Index.jsx'
-import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Footer from './Footer.jsx';
